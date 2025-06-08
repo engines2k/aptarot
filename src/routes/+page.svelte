0b1cc6208a4d04f045ad857650511949b6158e06
Zeke Barefoot <syncorex0@gmail.com>
2025-06-08 15:39:23 -0600

    Split card carousel into separate component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,98 +1,16 @@
 <script lang="ts">
 	import cardData from '$lib/card-data.json';
 	import CardInfo from './CardInfo.svelte';
-	import { onMount } from 'svelte';
-	import { gsap } from "gsap";
-	import { Observer } from 'gsap/all';
-	import { Draggable } from "gsap/Draggable";
+	import CardCarousel from './CardCarousel.svelte';
 
-	gsap.registerPlugin(Draggable);
-	gsap.registerPlugin(Observer)
-
-	let ogTarot = {
-		x: 0,
-		y: 0,
-		scale: 1,
-		rotation: 0
-	}
-	
-	let testOffsets = {
-		x: 0,
-		y: 0
-	}
-
-	const playingCard='/cards/card.png';
 	let selected = $state(-1);
-	let startY: number = 0;
 	let activeCard = $state({ id: -1, name: "No card selected", image: "/cards/card.png" });
 	let cards = $state(cardData);
 	
-	onMount(() => {
-		const spread = 30;
-		const tarotEls = document.querySelectorAll(".tarot-card");
-		const total = tarotEls.length;
-		const angleMapper =  gsap.utils.mapRange(0, total - 1, -spread / 2, spread / 2);
-	
-		const yMapper = function(index: number) {
-			const height = spread*2;
-			let x = index / (total-1);
-			let y = (4*x**2 - 4*x) * height;
-			return y;
-		};
-		
-		tarotEls.forEach((card, i) => {
-			gsap.set(card, {
-				rotation: angleMapper(i),
-				y: yMapper(i)
-			});
-		});
-		
-		Observer.create({
-			type: "wheel,touch,scroll",
-			onChangeY({ velocityY }) {
-				testOffsets.x += velocityY * 0.0002;
-			}
-		});
-
-		Draggable.create(".tarot-card", {
-			type: "x,y",
-			onPress: function() {
-				console.log(this)
-				ogTarot = {
-					x: this.x,
-					y: this.y,
-					scale: this.scale,
-					rotation: gsap.getProperty(this.target, "rotation") as number
-				}
-				gsap.to(this.target, {
-					scale: 1.1,
-					rotate: 5,
-					ease: "elastic.out(1, 0.5)",
-					duration: .6
-				});
-			},
-			onDrag: function() {
-				if (ogTarot.y - this.y > 100)
-					selected = this.target.id;
-				else 
-					selected = -1;
-			},
-			onRelease: function() {
-				gsap.to(this.target, {
-				scale: 1,
-				rotate: ogTarot.rotation,
-				x: Math.round(ogTarot.x + testOffsets.x), 
-				y: Math.round(ogTarot.y),
-				ease: "elastic.out(1, 0.3)", // Mimics elastic motion
-				duration: .6 
-				});
-				if (ogTarot.y - this.y > 100) {
-					startY = 0;
-					activeCard = cards[this.target.id] || { id: -1, name: "No card selected", image: "/cards/card.png" };
-				}
-			}
-		});
-	});
+	function changeCard(card: { id: number, name: string, image: string }) {
+		activeCard = card;
+		selected = card.id;
+	}
 </script>
 
 <svelte:head>
@@ -103,7 +21,7 @@
 <section>
 	<h1>
 		<CardInfo card={activeCard}/>
-		<div class="mt-12" style="display: flex; flex-wrap: nowrap; justify-content: center;">
+		<!-- <div class="mt-12" style="display: flex; flex-wrap: nowrap; justify-content: center;">
 			{#each cards as card, index (card.id)}
 			<div
 			class="tarot-card"
@@ -119,12 +37,17 @@
 				/>
 			</div>
 			{/each}
-		</div>
+		</div> -->
+		<CardCarousel 
+		{cards}
+		{activeCard}
+		{selected}
+		{changeCard}
+		/>
 	</h1>
 </section>
 
 <style>
-
 	section {
 		display: flex;
 		flex-direction: column;
@@ -136,26 +59,4 @@
 	h1 {
 		width: 100%;
 	}
-
-	.tarot-card {
-		padding:0;
-		max-width:125px;
-		margin: 0 auto;
-		user-select: none;
-	}
-
-	.tarot-card img {
-		image-rendering: pixelated;
-		padding:0;
-		margin:0;
-	}
-
-	.card-selected {
-		box-shadow: 6px 10px 89px -10px rgba(255,204,0);
-	}
-
-	.card-active {
-		box-shadow: 6px 10px 89px 0px rgb(42, 140, 231);
-	}
-
 </style>
---
+++ src/routes/CardCarousel.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    const playingCard='/cards/card.png';
+    let { cards, activeCard, selected, changeCard } = $props();
+    let startY: number = 0;
+	let ogTarot = {
+		x: 0,
+		y: 0,
+		scale: 1,
+		rotation: 0
+	};
+    import { gsap } from "gsap";
+    import { Observer } from 'gsap/all';
+    import { Draggable } from "gsap/Draggable";
+
+    gsap.registerPlugin(Draggable);
+    gsap.registerPlugin(Observer);
+
+    // For testing purposes
+	let testOffsets = {
+		x: 0,
+		y: 0
+	};
+
+
+    onMount(() => {
+		const spread = 30;
+		const tarotEls = document.querySelectorAll(".tarot-card");
+		const total = tarotEls.length;
+		const angleMapper =  gsap.utils.mapRange(0, total - 1, -spread / 2, spread / 2);
+	
+		const yMapper = function(index: number) {
+			const height = spread*2;
+			let x = index / (total-1);
+			let y = (4*x**2 - 4*x) * height;
+			return y;
+		};
+		
+		tarotEls.forEach((card, i) => {
+			gsap.set(card, {
+				rotation: angleMapper(i),
+				y: yMapper(i)
+			});
+		});
+		
+		Observer.create({
+			type: "wheel,touch,scroll",
+			onChangeY({ velocityY }) {
+				testOffsets.x += velocityY * 0.0002;
+			}
+		});
+
+		Draggable.create(".tarot-card", {
+			type: "x,y",
+			onPress: function() {
+				console.log(this)
+				ogTarot = {
+					x: this.x,
+					y: this.y,
+					scale: this.scale,
+					rotation: gsap.getProperty(this.target, "rotation") as number
+				}
+				gsap.to(this.target, {
+					scale: 1.1,
+					rotate: 5,
+					ease: "elastic.out(1, 0.5)",
+					duration: .6
+				});
+			},
+			onDrag: function() {
+				if (ogTarot.y - this.y > 100)
+					selected = this.target.id;
+				else 
+					selected = -1;
+			},
+			onRelease: function() {
+				gsap.to(this.target, {
+				scale: 1,
+				rotate: ogTarot.rotation,
+				x: Math.round(ogTarot.x + testOffsets.x), 
+				y: Math.round(ogTarot.y),
+				ease: "elastic.out(1, 0.3)", // Mimics elastic motion
+				duration: .6 
+				});
+				if (ogTarot.y - this.y > 100) {
+					startY = 0;
+					changeCard(cards[this.target.id] || { id: -1, name: "No card selected", image: "/cards/card.png" });
+				}
+			}
+		});
+	});
+</script>
+
+<div class="mt-12" style="display: flex; flex-wrap: nowrap; justify-content: center;">
+    {#each cards as card, index (card.id)}
+    <div
+    class="tarot-card"
+    id="{String(card.id)}"
+    >
+        <img
+        src={playingCard}
+        alt="Playing card"
+        width="100"
+        draggable="false"
+        class:card-active={index == activeCard.id}
+        class:card-selected={index == selected && index !== activeCard.id}
+        />
+    </div>
+    {/each}
+</div>
+
+<style>
+	.card-selected {
+		box-shadow: 6px 10px 89px -10px rgba(255,204,0);
+	}
+
+	.card-active {
+		box-shadow: 6px 10px 89px 0px rgb(42, 140, 231);
+	}
+</style>
